235f37c043df7479d51132ec27b925cf704481a4
cdelzell <claradelzell@gmail.com>
2024-09-24 10:13:53 -0500

    fixed assert post condition statements

--- pathing.py
+++ pathing.py
@@ -14,12 +14,12 @@ def set_current_graph_paths():
 def get_test_path():
     return graph_data.test_path[global_game_data.current_graph_index]
 
-
+##goes to random nodes
+##keeps track of if it has hit the target
+##if it has hit the target and is at the end node, it can exit
+##otherwise, it will keep generating nodes to visit
 def get_random_path():
-    ##goes to random nodes
-    ##keeps track of if it has hit the target
-    ##if it has hit the target and is at the end node, it can exit
-    ##otherwise, it will keep generating nodes to visit
+    # assert precondition
 
     graph = graph_data.graph_data[global_game_data.current_graph_index]
     start_node = graph[0]
@@ -46,10 +46,22 @@ def get_random_path():
     assert path is not None, "This path does not contain any nodes"
     assert path[0] == 0, "This path does not start at the correct node"
     assert path[len(path) - 1] == len(graph) - 1, "This path does not end at the correct node"
+    assert check_adjacent_nodes(path), "This path is not a connected path"
+
 
     return path
 
+## ensure that a path is made of sequential nodes all connected by edges
+# (i.e there are no breaks in the path)
+def check_adjacent_nodes(path):
+    graph = graph_data.graph_data[global_game_data.current_graph_index]
 
+    for i in range(0, len(path) - 2):
+        node_info = graph[path[i]]
+        if(not(path[i+1] in node_info[1])) :
+            return False
+    
+    return True
 
 def get_dfs_path():
     return [1,2]
