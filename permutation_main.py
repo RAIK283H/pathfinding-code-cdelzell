f04cc78a6d7eb87d9852f02251a893841abd7411
cdelzell <claradelzell@gmail.com>
2024-10-26 00:59:41 -0500

    comment added

--- permutation_main.py
+++ permutation_main.py
@@ -1,7 +1,7 @@
 import graph_data
 import permutation
 
-
+# Get all hamiltonian cycles for a given graph
 def displayHamiltonian(graphIndex):
     smallGraphHamiltonianCycles = permutation.findHamiltonian(graph_data.graph_data[graphIndex])
     print("Valid Hamiltonian Cycles for Graph " + str(graphIndex) + ": ")
